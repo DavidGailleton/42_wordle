b0688ddf560e7389961a9afcbc08d73df59ca7fe
David Gailleton <dgaillet@student.42lyon.fr>
2026-06-27 12:29:16 +0000

    class Game

--- src/game.py
+++ src/game.py
@@ -1,6 +1,8 @@
 import random
+from colorama import Back, Fore, Style
 
 
 class Game:
     def __init__(self, words: list[str]) -> None:
         self.word = random.choice(words)
+        self.tries: list[str] = []
