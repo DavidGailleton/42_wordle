a5067930a30bcf03bcca8bc47143eb17418f45bc
David Gailleton <dgaillet@student.42lyon.fr>
2026-06-28 18:19:42 +0000

    fix parsing only accept alphabetics

--- src/parsing.py
+++ src/parsing.py
@@ -5,10 +5,14 @@ def parsing(file: str) -> list[str]:
     words: list[str] = []
     with open(file) as f:
         for i, line in enumerate(f):
-            word = line.removesuffix("\n")
+            word = line.removesuffix("\n").lower()
             if len(word) != 5:
                 raise ParsingError(
-                    f'Line {i}: word "{word}" len not equal to 5'
+                    f'Line {i + 1}: word "{word}" len not equal to 5'
+                )
+            if not word.isalpha():
+                raise ParsingError(
+                    f'Line {i + 1}: word "{word}" should be only alphabetic'
                 )
             words.append(word)
     return words
